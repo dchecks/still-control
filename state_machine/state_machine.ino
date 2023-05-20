5bf68e5d2456a8d34b0234f0bf4ee12ee71775b7
Dan <dan.checketts@protonmail.ch>
2023-05-20 18:20:04 +1200

    Prestart test

--- state_machine/state_machine.ino
+++ state_machine/state_machine.ino
@@ -6,6 +6,7 @@
 #define MAX6675_SCK  7
 
 // TEMP Targets
+const int target_prestart = 40;
 const int target_low_temp = 70;
 const int target_high_temp = 75;
 
@@ -35,50 +36,72 @@ int position = 0;
 int movement = 0;
 
 void setup() {
+  print("Starting...")
+
   Serial.begin(9600);
+  Serial.println('Starting...')
+
   myStepper.setSpeed(60);
-  
-  // Close the valve
-  // myStepper.step(CLOSE * 1000);
-}
 
-double readThermocouple() {
-
-  uint16_t v;
+  # Thermo setup
   pinMode(MAX6675_CS, OUTPUT);
   pinMode(MAX6675_SO, INPUT);
   pinMode(MAX6675_SCK, OUTPUT);
   
-  digitalWrite(MAX6675_CS, LOW);
-  delay(1);
+}
 
-  // Read in 16 bits,
-  //  15    = 0 always
-  //  14..2 = 0.25 degree counts MSB First
-  //  2     = 1 if thermocouple is open circuit  
-  //  1..0  = uninteresting status
-  
-  v = shiftIn(MAX6675_SO, MAX6675_SCK, MSBFIRST);
-  v <<= 8;
-  v |= shiftIn(MAX6675_SO, MAX6675_SCK, MSBFIRST);
-  
-  digitalWrite(MAX6675_CS, HIGH);
-  if (v & 0x4) 
-  {    
-    // Bit 2 indicates if the thermocouple is disconnected
-    return NAN;     
+double readThermocouple(int avg_over_reads=1) {
+  int v_sum = 0;
+
+  for (int reads = 0; i < avg_over_reads; reads++){
+    uint16_t v;
+    digitalWrite(MAX6675_CS, LOW);
+    delay(1);
+
+    // Read in 16 bits,
+    //  15    = 0 always
+    //  14..2 = 0.25 degree counts MSB First
+    //  2     = 1 if thermocouple is open circuit  
+    //  1..0  = uninteresting status
+    
+    v = shiftIn(MAX6675_SO, MAX6675_SCK, MSBFIRST);
+    v <<= 8;
+    v |= shiftIn(MAX6675_SO, MAX6675_SCK, MSBFIRST);
+    
+    digitalWrite(MAX6675_CS, HIGH);
+    if (v & 0x4) 
+    {    
+      // Bit 2 indicates if the thermocouple is disconnected
+      return NAN;     
+    }
+
+    // The lower three bits (0,1,2) are discarded status bits
+    v >>= 3;
+
+    v_sum += v
+    delay(100) # ms
   }
 
-  // The lower three bits (0,1,2) are discarded status bits
-  v >>= 3;
-
   // The remaining bits are the number of 0.25 degree (C) counts
-  return v*0.25 + THERMO_COMPENSATE;
+    return (v_sum / avg_over_reads) * 0.25 + THERMO_COMPENSATE;
 }
 
 
 void preStart() {
-
+  polling_ms = 5000
+  Serial.println("Waiting for prestart temp to be reached... poll rate " + String(polling_ms) + "ms")
+  
+  // Average over 5 times so we're sure
+  prestart = readThermocouple(5)
+  while prestart < target_prestart {
+    String msg = "Target: " + String(target_prestart) + "c, Current: " + String(prestart) + "c"
+    Serial.println(msg)
+    delay(polling_ms);
+    prestart = readThermocouple(5)
+  }
+  String msg = "Target breached: " + String(target_prestart) + "c, Current: " + String(prestart) + "c"
+  Serial.println(msg)
+  Serial.println("Exiting prestart")
 }
 
 void start() {
@@ -127,15 +150,19 @@ void steadyState() {
 }
 
 void loop() {
-  int current_temp = readThermocouple()
-  if current_temp < 40 {
+  int current_temp = readThermocouple(2)
+
+  if current_temp < target_prestart {
+    Serial.println("Stage: Prestart")
     preStart()
   }
   else if current_temp < 70 {
+    Serial.println("Stage: Start")
     start()
   }
   else {
-    steadyState()
+    Serial.println("Stage: Steady State")
+    steadyState()  
   }
 }
 
